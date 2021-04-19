8f274076f09162927b4211f8798261a79f49298e
Matheus Oliveira <matheusalmeida.soliveira@gmail.com>
2021-04-19 17:55:06 -0300

    Classe Ferrari

--- estudo/src/main/java/heranca/desafio/Carro.java
+++ estudo/src/main/java/heranca/desafio/Carro.java
@@ -1,21 +1,21 @@
 package heranca.desafio;
 
 public class Carro {
-    private int velocidadeAtual;
+    protected int velocidadeAtual;
 
     public Carro(){
         this.velocidadeAtual = 0;
     };
 
 
-    public void Acelerar(Aceleracao aceleracao){
+    public void acelerar(Aceleracao aceleracao){
         if(aceleracao == Aceleracao.NORMAL)
             this.velocidadeAtual += 5;
         else if(aceleracao == Aceleracao.FORTE)
             this.velocidadeAtual += 10;
     }
 
-    public void Frear(){
+    public void frear(){
         if(this.velocidadeAtual >= 5){
             this.velocidadeAtual -= 5;
         }
---
+++ estudo/src/main/java/heranca/desafio/Ferrari.java
@@ -0,0 +1,15 @@
+package heranca.desafio;
+
+public class Ferrari extends Carro {
+    public Ferrari(){
+        super();
+    }
+
+    @Override
+    public void acelerar(Aceleracao aceleracao){
+        if(aceleracao == Aceleracao.NORMAL)
+            this.velocidadeAtual += 10;
+        else if(aceleracao == Aceleracao.FORTE)
+            this.velocidadeAtual += 15;
+    }
+}
