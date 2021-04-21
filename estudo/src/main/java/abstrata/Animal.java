e63a979131ef2ce424e28a34bce25cc7f8bf4e25
Matheus Oliveira <matheusalmeida.soliveira@gmail.com>
2021-04-21 14:28:24 -0300

    Abstrato

---
+++ estudo/src/main/java/abstrata/Animal.java
@@ -0,0 +1,10 @@
+package abstrata;
+
+public abstract class Animal {
+    
+    public String respirar(){
+        return "CO2";
+    }
+
+    public abstract double mover();
+}
---
+++ estudo/src/main/java/abstrata/Cachorro.java
@@ -0,0 +1,9 @@
+package abstrata;
+
+public class Cachorro extends Animal {
+    
+    @Override
+    public double mover() {
+        return 50;
+    }
+}
