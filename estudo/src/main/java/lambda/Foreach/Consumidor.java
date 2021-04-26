84c7f3abe8b5dad2e0e9320cfefe9dceb8973dd7
Matheus Oliveira <matheusalmeida.soliveira@gmail.com>
2021-04-25 21:42:32 -0300

    Function e funcoes aninhadas

--- estudo/src/main/java/lambda/Foreach/Consumidor.java
+++ estudo/src/main/java/lambda/Foreach/Consumidor.java
@@ -3,6 +3,7 @@ package lambda.Foreach;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class Consumidor {
     public static void main(String[] args) {
@@ -15,5 +16,20 @@ public class Consumidor {
 
         produtos.forEach(i -> imprimir.accept(i));
         produtos.forEach(imprimir);
+
+
+
+
+        Function<Double, Integer> funcao = (a) -> {
+            return (int)(a*0.5);
+        };
+
+        Function<Integer, String> resultado = numero -> {
+            return "O resultado é: " + numero;
+        };
+
+        System.out.println(funcao.andThen(resultado).apply(23.0)); 
+
+
     }
 }
