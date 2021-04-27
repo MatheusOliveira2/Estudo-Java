34711d0a81597ff64ce358f0f3e14d8ecfc7b265
Matheus Oliveira <matheusalmeida.soliveira@gmail.com>
2021-04-26 22:36:25 -0300

    Suplier

---
+++ estudo/src/main/java/lambda/Fornecedor.java
@@ -0,0 +1,14 @@
+package lambda;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
+
+public class Fornecedor {
+    public static void main(String[] args) {
+        
+        Supplier<List<String>> lista = () -> Arrays.asList("A", "B", "C");
+
+        System.out.println(lista.get());
+    }
+}
