530590644178b38441290c5cc761a2d9de49446c
Matheus Oliveira <matheusalmeida.soliveira@gmail.com>
2021-04-26 22:39:52 -0300

    Unary Operator

--- estudo/src/main/java/lambda/Fornecedor.java
+++ estudo/src/main/java/lambda/Fornecedor.java
@@ -3,6 +3,7 @@ package lambda;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Supplier;
+import java.util.function.UnaryOperator;
 
 public class Fornecedor {
     public static void main(String[] args) {
@@ -10,5 +11,12 @@ public class Fornecedor {
         Supplier<List<String>> lista = () -> Arrays.asList("A", "B", "C");
 
         System.out.println(lista.get());
+
+        UnaryOperator<Integer> maisDois = n -> n+2;
+        UnaryOperator<Integer> xDois = n -> n*2;
+        UnaryOperator<Integer> aoQuadrado = n -> n*n;
+
+        System.out.println(maisDois.andThen(xDois.andThen(aoQuadrado)).apply(1)); 
+        
     }
 }
