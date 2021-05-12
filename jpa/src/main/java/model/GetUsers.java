c4e2770ad8052ed6dc2364fb1e851bf9f2e2849f
Matheus Oliveira <matheusalmeida.soliveira@gmail.com>
2021-05-12 16:51:12 -0300

    Get user JPA

---
+++ jpa/src/main/java/model/GetUsers.java
@@ -0,0 +1,16 @@
+package model;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class GetUsers {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa");
+        EntityManager em = emf.createEntityManager();
+
+        User user = em.find(User.class, 1L);
+        System.out.println(user.getName());
+        em.close();
+    }
+}
